576e7865d4d91ec0672b564d9549cb7b806c7ebb
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-16 18:56:44 -0500

    user can now input the number of characteres and confirm selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,35 @@
-// Assignment code here
+// Creating the main function that will execute once button is
+
+// 1 FUNCTION TO DO IT ALL! generatePassword();
+    // starts interacition with user and returns password on screen
+
+    //1st interaction: Criteria Selection
+    // 2nd interaction: Determining Length
+    // 3rd interaction: Determining Acceptable Char types
+      // upper case, lower case, numeric, special characters
+    // 4th interaction: Validation of character minimal of 1 character type selected
 function generatePassword() {
-  var randomKey = 1; 
-  return randomKey; 
+  var randomKey = CharCount(); 
+  return randomKey;
 }
 
+  // generate 1st alert window
 
+var CharCount = function() {
+var userCharNum = 0;
+
+  while(userCharNum < 8 || userCharNum > 128) {
+    userCharNum = window.prompt('How long do you want your password to be? (Min: 8 - Max: 126) ');
+    if(userCharNum < 8 || userCharNum > 128) {
+      window.alert("Your password size cannot be smaller than 8 or bigger than 128 characters");
+    }
+    else {
+      console.log(userCharNum);
+      window.confirm("You selected " + userCharNum + " characters. Correct?");
+      break;
+    }
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 

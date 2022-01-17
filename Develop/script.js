d9965e855b29f0c8d253b9d2cfa51dd3b4d2a968
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-01-17 15:53:00 -0500

    all code functionality works. cleaning work needed

--- Develop/script.js
+++ Develop/script.js
@@ -19,27 +19,21 @@
 
 var PassCharCount = 0;
 var CharTypes = [false, false, false, false]; // [Uppder Case, Lower Case, Number, Special Characters]
-var Password = new Array(128);
-
-const allLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const allUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const allNumbers = ["0","1" ,"2", "3","4","5","6","7","8","9"];
-const allSpecialChar = ["!","@","#","$","%","^","&","*","+","-"];
-var count = 0;
-var typesUsedCount = 0;
-
 var lowerCaseRando = false;
 var UpperCaseRando = false;
 var NumbersRando = false;
 var SpecialRando = false;
 
 
-/*---------------------------PASSWORD GENERATOR USING USER INPUTS--------------------------------*/
+/*---------------------------Approach 1: PASSWORD GENERATOR ------------------
 
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
+  //const allLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //const allUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  //const allNumbers = ["0","1" ,"2", "3","4","5","6","7","8","9"];
+  //const allSpecialChar = ["!","@","#","$","%","^","&","*","+","-"];
+  //var count = 0;
+  //var typesUsedCount = 0;
+----------------------------------------------------------------------------------------------------
 
 function randomizeCharacters() {
 
@@ -105,16 +99,112 @@ function randomizeCharacters() {
   console.log(PassCharCount);
   return answer;
 }
+----------------------------------------------------------------------------------------------*/
+/* --------------------Approach 2: PASSWORD GENERATOR ------------------------------*/
 
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
 
+function generate_password(password_length) {
+
+  var number_chars = '0123456789';
+  var upper_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lower_chars = 'abcdefghijklmnopqrstuvwxyz';
+  var special_chars = '!@#$%^&*+-';
+  var all_chars = number_chars + upper_chars + lower_chars + special_chars;
+  var rand_pass_arr = new Array(password_length);
+  var pass_length = rand_pass_arr[0];
+  var result_arr = new Array(password_length);
+  rand_pass_arr[0] = number_chars;
+  rand_pass_arr[1] = upper_chars;
+  rand_pass_arr[2] = lower_chars;
+  rand_pass_arr[3] = special_chars;
+  rand_pass_arr = rand_pass_arr.fill(all_chars, 4);
+  console.log(rand_pass_arr);
+
+  // until this point the function organized an array with each type of char per column. 
+  // next I would have to search only the columns approved by user to be used in the random key generation.
+
+  var finalKey = "";
+  var i = 0;
+  while (i < pass_length){
+
+    if(CharTypes[0] === true){
+      result_arr[i] = number_chars[getRndInteger(1, number_chars.length)];
+      console.log(result_arr);
+      i++;
+    }
+    else if(i === pass_length){
+      console.log("AQUI PAROU");
+      finalKey = result_arr.join();
+      console.log(finalKey);
+      return finalKey;
+    }
+    else{}
+
+    if(CharTypes[1] === true && i < pass_length){
+      result_arr[i] = upper_chars[getRndInteger(1, upper_chars.length)];
+      console.log(result_arr);
+      i++;
+    }
+    else if(i === pass_length){
+      console.log("AQUI PAROU");
+      finalKey = result_arr.join();
+      console.log(finalKey);
+      return finalKey;
+    }
+    else{}
+    if(CharTypes[2] === true && i < pass_length){
+      result_arr[i] = lower_chars[getRndInteger(1, lower_chars.length)];
+      console.log(result_arr);
+      i++;
+    }
+    else if(i === pass_length){
+      console.log("AQUI PAROU");
+      finalKey = result_arr.join();
+      console.log(finalKey);
+      return finalKey;
+    }
+    else{}
+    if(CharTypes[3] === true && i < pass_length){
+      result_arr[i] = special_chars[getRndInteger(1, special_chars.length)];
+      console.log(result_arr);
+      i++;
+    }
+    else if(i === pass_length){
+      console.log("AQUI PAROU");
+      finalKey = result_arr.join();
+      console.log(finalKey);
+      return finalKey;
+    }
+    else{}
+  }
+  finalKey = result_arr.join('').trim();
+
+  return finalKey;
+}
+ /*output = shuffle_array(rand_pass_arr.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join(''); 
+  document.getElementById('output').value = output; 
+
+function shuffle_array(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+*/
 /* -----------------MAIN FUNCTION ---------------------------------------------------------*/
 
 function generatePassword() {
   PassCharCount = CharCount(); 
   console.log(PassCharCount);
   defineCharTypes();
-  console.log(CharTypes);
-  Password = randomizeCharacters(); 
+  console.log(CharTypes); /*dont remove - shows user selection of types */
+  var Password = generate_password(PassCharCount);
   console.log(Password);
   return Password;
   }
@@ -136,7 +226,6 @@ var CharCount = function() {
     }
     else {
       window.confirm("You selected " + userCharNum + " characters. Correct?");
-      console.log(userCharNum);
       PassCharCount = userCharNum;
       return PassCharCount;
     }
@@ -164,7 +253,6 @@ function defineCharTypes() {
     }
     else{
       window.confirm("You Have selected: "  + "\nUpper Case: " + CharTypes[0] + "\nLower Case: " + CharTypes[1] + "\nNumbers: " + CharTypes[2] + "\nSpecial Characters: " + CharTypes[3]);
-      console.log(CharTypes);
       return CharTypes;
     }
   }
